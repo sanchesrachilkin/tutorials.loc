4b98542edd55a953c4d1fc151633de20fbc1a390
potroshitelnica <staciayakovleva@gmail.com>
2018-02-11 01:33:10 +0300

    Нижний сайтбар с менюшками

---
+++ examle.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ася
+ * Dat
+ */
+
+?>
+<!-- News item template -->
+<div class="newsitem">
+    <div class="newsitem__img">
+        <img src="Новости['путь к картинке']">
+    </div>
+    <div class="newsitem__content">
+        <div class="newsitem__header">
+            Новости['Название']
+        </div>
+        <div class="newsitem__text">
+            Новости['Текст']
+        </div>
+    </div>
+</div>
+<!-- News item template end-->
+
+<div class="newsitem">
+    <div class="newsitem__img">
+        <img src="Новости['путь к картинке']">
+    </div>
+    <div class="newsitem__content">
+        <div class="newsitem__header">
+            Новости['Название']
+        </div>
+        <div class="newsitem__text">
+            Новости['Текст']
+        </div>
+    </div>
+</div>
+<div class="newsitem">
+    <div class="newsitem__img">
+        <img src="Новости['путь к картинке']">
+    </div>
+    <div class="newsitem__content">
+        <div class="newsitem__header">
+            Новости['Название']
+        </div>
+        <div class="newsitem__text">
+            Новости['Текст']
+        </div>
+    </div>
+</div>
+<div class="newsitem">
+    <div class="newsitem__img">
+        <img src="Новости['путь к картинке']">
+    </div>
+    <div class="newsitem__content">
+        <div class="newsitem__header">
+            Новости['Название']
+        </div>
+        <div class="newsitem__text">
+            Новости['Текст']
+        </div>
+    </div>
+</div>
+<div class="newsitem">
+    <div class="newsitem__img">
+        <img src="Новости['путь к картинке']">
+    </div>
+    <div class="newsitem__content">
+        <div class="newsitem__header">
+            Новости['Название']
+        </div>
+        <div class="newsitem__text">
+            Новости['Текст']
+        </div>
+    </div>
+</div>
+<div class="newsitem">
+    <div class="newsitem__img">
+        <img src="Новости['путь к картинке']">
+    </div>
+    <div class="newsitem__content">
+        <div class="newsitem__header">
+            Новости['Название']
+        </div>
+        <div class="newsitem__text">
+            Новости['Текст']
+        </div>
+    </div>
+</div>
+<div class="newsitem">
+    <div class="newsitem__img">
+        <img src="Новости['путь к картинке']">
+    </div>
+    <div class="newsitem__content">
+        <div class="newsitem__header">
+            Новости['Название']
+        </div>
+        <div class="newsitem__text">
+            Новости['Текст']
+        </div>
+    </div>
+</div>
